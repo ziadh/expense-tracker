b61b0a976b7c9cad2879f56fd5107ef2d098949e
Ziad <zhussein1996@gmail.com>
2024-02-26 11:37:09 -0500

    add files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import { Container, Flex, Box } from "@chakra-ui/react";
+import Main from "./components/main";
+function App() {
+  return (
+    <Container bg={"f8fafd"} maxW={"Container.3xl"} height={"100vh"} p={"0"}>
+      <Flex height={"full"}>
+        <Box height={"full"} flex={5} w={["20%", "30%", "20%", "50%", "60%"]}>
+          <Main />
+        </Box>
+      </Flex>
+    </Container>
+  ); //
+}
+
+export default App;
